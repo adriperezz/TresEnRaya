cc92c8436a7d3fcdb5a3ffd9301c716b5b9b79ba
adriperezz <adriperez@wolfonline.es>
2023-03-30 20:43:30 +0200

    copiando el proyecto entero

---
+++ package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"m":"tresEnRaya","l":"tresEnRaya"}];updateSearchResults();
---
+++ type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"tresEnRaya","l":"celda"},{"p":"tresEnRaya","l":"ganador"},{"p":"tresEnRaya","l":"jugador"},{"p":"tresEnRaya","l":"mainsito"},{"p":"tresEnRaya","l":"partida"},{"p":"tresEnRaya","l":"tablero"}];updateSearchResults();
